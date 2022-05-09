04fec5708ecd67c37c5741bb9376a5e328c3ca0c
Julio2DAW <jramosgago.guadalupe@alumnado.fundacionloyola.net>
2022-05-09 12:29:27 +0200

    archivos de la app

---
+++ app/php/controlador.php
@@ -0,0 +1,14 @@
+<?php
+    /**
+     * @file controlador.php
+     * Archivo para crear el controlador de la app.
+     * @author Julio Antonio Ramos Gago (jramosgago.guadalupe@alumnado.fundacionloyola.net)
+     * @license GPLv3 2022.
+     */
+
+    class Controlador{
+
+        public $modelo;
+
+        function 
+    }
---
+++ app/php/modelo.php
@@ -0,0 +1,17 @@
+<?php
+
+    /**
+     * @file modelo.php
+     * Archivo para crear el modelo de la app.
+     * @author Julio Antonio Ramos Gago (jramosgago.guadalupe@alumnado.fundacionloyola.net)
+     * @license GPLv3 2022.
+     */
+
+    /**
+     * @class Modelo{}
+     * Clase Modelo, para realizar consultas con la base de datos.
+     */
+    class Modelo{
+
+        
+    }
